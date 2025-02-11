014abf685dff9bddeb91ead09c5f143ac7b16954
CLEYTONMACHADO <cleytonmachado2010@gmail.com>
2025-02-11 17:42:18 -0300

    First commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,20 @@
+describe('Orange HRM Tests', () => {
+  it('Login - Sucesso', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get("[type='submit']").click()
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+  })
+  it('Login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin123')
+    cy.get("[name='password']").type('admin1234')
+    cy.get("[type='submit']").click()
+    cy.get('.oxd-alert')
+
+    //cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+  })
+
+
+})
