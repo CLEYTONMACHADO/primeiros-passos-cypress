f2492afc821fd7f762c71ca03d8b8c792e0da872
CLEYTONMACHADO <cleytonmachado2010@gmail.com>
2025-02-14 17:12:42 -0300

    Upadate user

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,70 @@
+import userData from '../fixtures/userData.json'
+
+describe('Orange HRM Tests', () => {
+
+  const selectorList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
+    wrongCredentialAlert: "[role='alert']",
+    dashboardGrid: ".orangehrm-dashboard-grid",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericItem:".oxd-input--active",
+    genericItemDropDow:".oxd-select-text",
+    closeButton:".--close",
+    submitButton: "[type='submit']"
+    
+  }
+
+  it.only('User Info Upadate - Sucesso', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorList.dashboardGrid)
+    cy.get(selectorList.myInfoButton).click()
+    cy.get(selectorList.firstNameField).clear().type('Primeiro Nome')
+    cy.get(selectorList.lastNameField).clear().type('Nick Name')
+    cy.get(selectorList.genericItem).eq(3).clear().type('0123')
+    cy.get(selectorList.genericItem).eq(4).clear().type('321')
+    cy.get(selectorList.genericItem).eq(5).clear().type('1234')
+    cy.get(selectorList.genericItem).eq(6).clear().type('2025-14-02')
+    cy.get(selectorList.closeButton).click()
+    cy.get(selectorList.submitButton).eq(0).click()
+    cy.get('.oxd-toast-close')
+    // cy.get(selectorList.genericItemDropDow).eq(0).type('Brazilian')
+
+
+  })
+  it('Login - Fail', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorList.usernameField).type(userData.userFail.username)
+    cy.get(selectorList.passwordField).type(userData.userFail.password)
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.wrongCredentialAlert)
+  })
+
+  //   it('Login - Sucesso', () => {
+  //   cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+  //   cy.get("[name='username']").type('Admin')
+  //   cy.get("[name='password']").type('admin123')
+  //   cy.get("[type='submit']").click()
+  //   cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+  // })
+  // it('Login - Fail', () => {
+  //   cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+  //   cy.get("[name='username']").type('Admin123')
+  //   cy.get("[name='password']").type('admin1234')
+  //   cy.get("[type='submit']").click()
+  //   cy.get('.oxd-alert')
+
+    
+  // })
+
+
+
+})
